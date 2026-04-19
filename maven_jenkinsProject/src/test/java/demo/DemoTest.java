ebba15810f4e8b1d61626e3e58ddb69761da4c02
sandeepsingh151617 <sandeepsingh151617@gmail.com>
2026-04-19 14:43:08 +0530

    First Commit

---
+++ maven_jenkinsProject/src/test/java/demo/DemoTest.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void case11() {
+		System.out.println("Case 11 completed");
+	}
+	@Test
+	public void case12() {
+		System.out.println("Case 12 completed");
+	}
+	@Test
+	public void case13() {
+		System.out.println("Case 13 completed");
+	}
+
+}
---
+++ maven_jenkinsProject/src/test/java/sample/SampleTest.java
@@ -0,0 +1,19 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void case1() {
+		System.out.println("Case 1 completed");
+	}
+	@Test
+	public void case2() {
+		System.out.println("Case 2 completed");
+	}
+	@Test
+	public void case3() {
+		System.out.println("Case 3 completed");
+	}
+
+}
