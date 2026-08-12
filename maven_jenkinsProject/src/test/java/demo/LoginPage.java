deb798b2854b3885ce22a1a7db1e0103e16a923f
Admin <Admin@DESKTOP-5M28M5I>
2026-08-12 20:10:33 +0530

    changes in demo package.

---
+++ maven_jenkinsProject/src/test/java/demo/LoginPage.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginPage {
+	WebDriver driver=null;
+	@Test
+	
+	
+	public void loginFlipkart() {
+		driver=new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+	}
+	@Test
+	public void logoutFlipkart() {
+		driver.quit();
+	}
+}
